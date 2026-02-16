a4f4a0a44366ec72b125ecb3da11052fc80b3084
Flavio Costa <flavionegocios2004@gmail.com>
2026-02-16 15:14:53 -0300

    Adiciona display grid e organiza colunas da página.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { Header } from "./components/Header";
+import styles from "./App.module.css";
+import { Post } from "./Post";
 
 function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main>
+          <Post author="Flávio" content="Hello World!" />
+          <Post author="Maria" content="How are you?" />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
